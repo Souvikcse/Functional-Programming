39610ab33f54e6f2377dfe4f5f578302bbf1256a
Souvikcse <sayanwinz@gmail.com>
2019-10-10 22:20:52 +0530

    Sliding window algorithm
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- CodingBlocks/StringWindow.cpp
+++ CodingBlocks/StringWindow.cpp
@@ -12,9 +12,40 @@ int main(){
         cout<<"No string";
         return 0;
     }
+    int left=0, right=0;
     unordered_map<char, int> umap;
+    unordered_map<char, int> vmap;
+    int window=0;
     REP(0, s2.length(), 1){
-        umap[s2[i]]=1;
+        umap[s2[i]]++;
+     }
+    window= umap.size();
+    string ans="";
+    int min=INT_MAX;
+    while (right<s1.length())
+    {
+        if(vmap.size() < window){
+            if(umap.count(s1[right]) > 0){
+                vmap[s1[right]]++;
+            }
+            right++;
+        }
+        else{
+            if(min > right-left){
+                min=right-left;
+                ans=s1.substr(left, min);
+            }
+            if(umap.count(s1[left]) > 0){
+                vmap[s1[left]]-=1;
+                if (vmap[s1[left]]==0){
+                   vmap.erase(s1[left]);
+                }   
+            }
+            left++;
+        }
     }
-    
+    if(ans.compare("")==0)
+        cout<<"No string";
+    else
+        cout<<ans; 
 }
