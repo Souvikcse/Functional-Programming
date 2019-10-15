4cdfbcfe44af8249e92d7672e775f461ceba6882
Souvikcse <sayanwinz@gmail.com>
2019-10-15 22:20:25 +0530

    recursion for kids
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- BasicC++/test9.cpp
+++ BasicC++/test9.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
-int main(){
-    int p;
-    int buf[10]={1,2,3,4,5,6,9,8};
-    cout<<buf[8];
+void print(int i, int n, int open, int close, char out[]){
+	if(i==2*n){
+        out[i]='\0';
+		cout<<out<<" "<<strlen(out)<<"\n";
+        return;
+	}
+    if(open<n){
+        out[i]='(';
+        print(i+1, n, open+1, close, out);
+    }
+    if(close<open){
+        out[i]=')';
+        print(i+1, n, open, close+1, out);
+    }
+}
+int main() {
+    char output[100];
+	int n;
+	cin>>n;
+	print(0, n, 0, 0, output);
 }
