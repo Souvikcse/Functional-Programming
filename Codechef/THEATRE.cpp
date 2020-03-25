902a2e0a9503ca1c5cd60aeed663d2eace6b7d02
Souvikcse <sayanwinz@gmail.com>
2020-03-26 01:43:35 +0530

    November 2019 Long
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- Codechef/THEATRE.cpp
+++ Codechef/THEATRE.cpp
@@ -1,35 +1,32 @@
 #include<iostream>
 #include<cstring>
+#include<algorithm>
 typedef long long ll;
 using namespace std;
 
 int freq[4][4];
 int cost[4];
-bool check[4];
 
 int MaxProfit(){
-    int sum = 0, slot = 0;
-    int ans = 0;
-    for(int i=0;i<4;i++){
-        slot|= 1<<i;
-        for(int j=0;j<4;j++){
-            if((slot&(1<<j))==0){
-                slot|= 1<<j;
-                for(int k=0;k<4;k++){
-                    if((slot&(1<<k))==0){
-                        slot|= 1<<k;
-                        for(int l=0;l<4;l++){
-                            if((slot&(1<<l))==0){
-                                slot|= 1<<l;
-
-                            }
-                        }
-                    }
-                }
-            }
+    string slot = "0123";
+    int f[4];
+    int ans = -1e9;
+    for(int i=0;i<24;i++){
+        int sum = 0;
+        for(int i=0;i<4;i++){
+            f[i] = freq[i][slot[i]-'0'];        //Assign movie A,B,C,D to slot[i] p.m.
         }
-        slot = 0;
+        sort(f, f+4);
+        for(int i=0;i<4;i++){
+            if(f[i]==0)
+                sum -= 100;
+            else
+                sum += (i+1)*25*f[i];
+        }
+        ans = max(ans, sum);
+        next_permutation(slot.begin(), slot.end());
     }
+    return ans;
 }
 int main(){
     cost[0]=100, cost[1]=75, cost[2]=50, cost[3]=25;
@@ -43,23 +40,17 @@ int main(){
 
         int n;
         cin>>n;
-        char m; int t;
+        string s; int ti;
         int profit = 0;
         for(int i=0;i<n;i++){
-            cin>>m>>t;
-            t = t!=12? t+12:t;
-            freq[m-'A'][(t-12)/3]++;
+            cin>>s>>ti;
+            char m = s[0];
+            ti = ti!=12? ti+12:ti+0;
+            freq[m-'A'][(ti-12)/3]++;
         }
-        memset(check, false, sizeof(check));
-        for(int i=0;i<4;i++)
-            for(int j=0;j<4;j++)
-                if(freq[i][j])   check[i] = true;
-
-        for(int i=0;i<4;i++)
-            if(!check[i])  profit -= 100;
-
         profit += MaxProfit();
         cout<<profit<<"\n";
+        total_profit += profit;
     }
     cout<<total_profit<<endl;
 }
